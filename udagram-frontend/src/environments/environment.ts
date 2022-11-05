b5b518d0f6465923ac18580299dd1e41f44d5695
lawalgodwin <godwinl200@gmail.com>
2022-11-05 22:15:58 +0100

    Add K8s deployment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af4ebc55998184a109b1d6e63233eccb-2097440014.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
